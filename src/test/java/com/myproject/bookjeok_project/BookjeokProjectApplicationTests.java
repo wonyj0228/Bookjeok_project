349926d6ddde13a23fe5ef9c0df22e3e90ff6fe0
Yejin Won <wonyj0228@naver.com>
2022-12-06 20:02:10 +0900

    스프링 프로젝트 파일 최초 커밋
    프로젝트 파일 생성 후 gitignore 작성 완료
    
    * gitignore 적용내용 : `java`, `intelliJ + all`, `gradle`, `macOS`

---
+++ src/test/java/com/myproject/bookjeok_project/BookjeokProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.bookjeok_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookjeokProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
