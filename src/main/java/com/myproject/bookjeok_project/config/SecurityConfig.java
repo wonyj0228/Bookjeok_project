f2625fa903c80bd86879056bcf81cde35351390a
Yejin Won <wonyj0228@naver.com>
2022-12-10 23:39:52 +0900

    #6 - Spring Security + thymeleaf 환경설정
    - SecurityConfig 작성 (TODO 기능 개발 후 수정 필요)
    - thymeleaf classpath 설정

---
+++ src/main/java/com/myproject/bookjeok_project/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.myproject.bookjeok_project.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests().anyRequest().permitAll() // TODO : 메인페이지, login, signIn만 permitAll
+                .and()
+                .formLogin();
+
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/images/**", "/js/**", "/webjars/**");
+    }
+
+
+
+}
