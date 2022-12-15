cb1803fe6cf83196d934617d0d5cfa33b3cf81c7
Yejin Won <wonyj0228@naver.com>
2022-12-15 23:34:37 +0900

    #7 - MainController 작성
    .

---
+++ src/main/java/com/myproject/bookjeok_project/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.myproject.bookjeok_project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String main() {
+        return "index";
+    }
+}
