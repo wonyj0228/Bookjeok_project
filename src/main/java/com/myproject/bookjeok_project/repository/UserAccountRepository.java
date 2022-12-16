35637d00c18c71be390351da5a07fe0a5ddd2441
Yejin Won <wonyj0228@naver.com>
2022-12-17 04:34:20 +0900

    #7 - 회원가입 구현 완료
    - 중복체크 및 체크 후 수정시 가입이 정상적으로 되는 것을 방지하기 위해서 이벤트를 추가했다.
    - 비밀번호 confirm을 통해 입력한 비밀번호를 두번 체킹할 수 있도록 이벤트를 추가했다.
    - 저장 전 모든 확인사항에 대해서 더블체킹 기능 추가했다.
    - 성공적으로 회원가입 완료시 login화면으로 redirect
    
    * TODO = 회원가입 완료창을 추가하거나 alert으로 회원가입이 완료되었음을 사용자가 확인할 수 있었으면 좋겠다.

--- src/main/java/com/myproject/bookjeok_project/repository/UserAccountRepository.java
+++ src/main/java/com/myproject/bookjeok_project/repository/UserAccountRepository.java
@@ -4,6 +4,6 @@ import com.myproject.bookjeok_project.domain.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
-    UserAccount findByEmail(String email); //중복검사
-    UserAccount findByNickname(String nickname); //중복검사
+    UserAccount findByEmail(String email);
+    UserAccount findByNickname(String nickname);
 }
